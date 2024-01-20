9bd70f1e63eb41a155b864b82bec2902aa4347b1
AParzival <agostini96@icloud.com>
2024-01-20 13:04:25 -0600

    104

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,37 +1,25 @@
-import React, { Fragment } from 'react';
-import { Link, Navigate } from 'react-router-dom';
+import React from 'react';
+import { Navigate } from 'react-router-dom';
 import logo from '../../images/logo1.jpg';
 import personWithPhone from '../../images/persononphone.png';
 import backGround2 from '../../images/background2.jpg';
 import '../../Styles.css';
 
 const Landing = ({ isAuthenticated }) => {
-    console.log("landing");
-    if (isAuthenticated) {
-        
-		return <Navigate to="/dashboard" />;
-	}
+  if (isAuthenticated) {
+    return <Navigate to="/dashboard" />;
+  }
 
-	return (
-		<Fragment>
-			<header className="App-header">
-        <div className="welcomeBox">
-        <h1>
-          Insta John Main Page
-        </h1>
-        </div>
-        <div className="discription">
-          This site is for people who know John
-          </div>
-       <div>   
-       <img src={personWithPhone} className='persononphone' alt='person with phone'/>
-       <img src={backGround2} className='Landingbackground' alt='landing background'/>
-       </div>
-      </header>
-		</Fragment>
-	);
+  return (
+    <div className="landing-container">
+      <h1>Insta John Main Page</h1>
+      <p>This site is for people who know John</p>
+      <div>
+        <img src={personWithPhone} className="persononphone" alt="person with phone" />
+        <img src={backGround2} className="landing-background" alt="landing background" />
+      </div>
+    </div>
+  );
 };
 
-
 export default Landing;
-
--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,22 +1,26 @@
-
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../images/logo1.jpg';
 import '../../Styles.css';
-const NavBar = () => {
-    return (
-        <div className='navbar'>
-            <nav>
-          <ul>
-          
-            <img src={logo} className='headerlogo' alt='logo in header'/>
-            <li><Link to="/landing">Landing</Link></li>
-            <li><Link to="/profile">Profile</Link></li>
 
-            </ul>
-          </nav>
-          
-        </div>
-    )
-}
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <nav>
+        <ul>
+          <li>
+            <img src={logo} className="headerlogo" alt="logo in header" />
+          </li>
+          <li>
+            <Link to="/landing">Landing</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
 export default NavBar;
