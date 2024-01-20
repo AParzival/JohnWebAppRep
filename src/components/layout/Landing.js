b74899da710c96ba8d2202e5a7b096eb18d5dd90
AParzival <agostini96@icloud.com>
2024-01-20 17:59:00 -0600

    1

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,3 +1,5 @@
+// Landing.jsx
+
 import React, { Fragment } from 'react';
 import { Link, Navigate } from 'react-router-dom';
 import logo from '../../images/logo1.jpg';
@@ -8,30 +10,25 @@ import '../../Styles.css';
 const Landing = ({ isAuthenticated }) => {
     console.log("landing");
     if (isAuthenticated) {
-        
-		return <Navigate to="/dashboard" />;
-	}
+        return <Navigate to="/dashboard" />;
+    }
 
-	return (
-		<Fragment>
-			<header className="App-header">
-        <div className="welcomeBox">
-        <h1>
-          Insta John Main Page
-        </h1>
-        </div>
-        <div className="discription">
-          This site is for people who know John
-          </div>
-       <div>   
-       <img src={personWithPhone} className='persononphone' alt='person with phone'/>
-       <img src={backGround2} className='Landingbackground' alt='landing background'/>
-       </div>
-      </header>
-		</Fragment>
-	);
+    return (
+        <Fragment>
+            <div className="Landing-container">
+                <header className="Landing-header">
+                    Insta John Main Page
+                </header>
+                <div className="Landing-description">
+                    This site is for people who know John
+                </div>
+                <div className="Landing-images">
+                    <img src={personWithPhone} className='persononphone' alt='person with phone' />
+                    <img src={backGround2} className='Landingbackground' alt='landing background' />
+                </div>
+            </div>
+        </Fragment>
+    );
 };
 
-
 export default Landing;
-
