630e896ea43dbf8ea6f242ede560a4964ad4cf9e
AParzival <agostini96@icloud.com>
2024-01-20 16:20:42 -0600

    4:20

--- src/components/layout/Profile.js
+++ src/components/layout/Profile.js
@@ -1,70 +1,69 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../../Styles.css';
+import '../../scripts/profile.js';
 import backgroundImage from '../../images/background2.jpg';
 
+
 const Profile = () => {
-  const SERVER_ROOT_URL = "https://john-webapp.azurewebsites.net/";
+    const SERVER_ROOT_URL="https://john-webapp.azurewebsites.net/"
+    console.log(SERVER_ROOT_URL);
+    const submitData = async () => {
+        const data = {
+            lastName: document.getElementById("lastName").value,
+            firstName: document.getElementById("firstName").value,
+            location: document.getElementById("location").value,
+            interest: document.getElementById("interest").value,
+            email: document.getElementById("email").value,
+            password: document.getElementById("password").value,
 
-  const [formData, setFormData] = useState({
-    lastName: '',
-    firstName: '',
-    location: '',
-    interest: '',
-    email: '',
-    password: '',
-  });
+        }
+        console.log("DATA", data);
+        const resp = await fetch(SERVER_ROOT_URL + "/profile", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        const json = await resp.json();
+        console.log(json);
+        alert(json.message);
+    }
 
-  const handleInputChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: e.target.value });
-  };
-
-  const submitData = async () => {
-    console.log("DATA", formData);
-    
-    const resp = await fetch(SERVER_ROOT_URL + "/profile", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
-
-    const json = await resp.json();
-    console.log(json);
-    alert(json.message);
-  };
-
-  return (
-    <div className="formbox">
-      <form>
-        <h1 className="homeh1">Profile Sign Up</h1>
-
-        <div className="input">
-          <div className="inputfield">
-            <input type="text" placeholder="First Name" id="firstName" onChange={handleInputChange} />
-          </div>
-          <div className="inputfield">
-            <input type="text" placeholder="Last Name" id="lastName" onChange={handleInputChange} />
-          </div>
-          <div className="inputfield">
-            <input type="text" placeholder="Location" id="location" onChange={handleInputChange} />
-          </div>
-          <div className="inputfield">
-            <input type="text" placeholder="Interests" id="interest" onChange={handleInputChange} />
-          </div>
-          <div className="inputfield">
-            <input type="email" placeholder="Email Address" id="email" onChange={handleInputChange} />
-          </div>
-          <div className="inputfield">
-            <input type="password" placeholder="Password" id="password" onChange={handleInputChange} />
-          </div>
-          <div className="subbutton">
-            <input type="button" value="Submit" onClick={submitData} />
-          </div>
-        </div>
-      </form>
+    return (
+       
+        <div className="formbox">
+        <form>
+            <h1 className="homeh1">Profile Sign Up</h1>
+            
+            <div className="input">
+                <div className="inputfield">
+                    <input type="text" placeholder="First Name" id="firstName"/>
+                </div>
+                <div className="inputfield">
+                    <input type="text" placeholder="Last Name" id="lastName"/>
+                </div>
+                <div className="inputfield">
+                    <input type="text" placeholder="Location" id="location"/>
+                </div>
+                <div className="inputfield">
+                    <input type="text" placeholder="Interests" id="interest"/>
+                </div>
+                <div className="inputfield">
+                    <input type="email" placeholder="Email Address" id="email"/>
+                </div>
+                <div className="inputfield">
+                    <input type="password" placeholder="Password" id="password" />
+                </div>
+                <div className="subbutton">
+                        <input type="button" value="Submit" onClick={ submitData} />
+                </div>
+            </div>
+        </form>
     </div>
-  );
-};
 
-export default Profile;
+    )
+}
+
+ 
+export default Profile
