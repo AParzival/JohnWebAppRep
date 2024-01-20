d3181cde8f6250527f04793535a1fdb1f82a7b3e
AParzival <agostini96@icloud.com>
2024-01-20 12:24:48 -0600

    1224

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -8,27 +8,30 @@ import '../../Styles.css';
 const Landing = ({ isAuthenticated }) => {
     console.log("landing");
     if (isAuthenticated) {
-        return <Navigate to="/dashboard" />;
-    }
+        
+		return <Navigate to="/dashboard" />;
+	}
 
-    return (
-        <Fragment>
-            <header className="App-header">
-                <div className="welcomeBox">
-                    <h1>
-                        Insta John Main Page
-                    </h1>
-                </div>
-                <div className="discription">
-                    This site is for people who know John
-                </div>
-                <div className="Landingbackground">
-                    <img src={backGround2} alt='landing background'/>
-                    <img src={personWithPhone} className='persononphone' alt='person with phone'/>
-                </div>
-            </header>
-        </Fragment>
-    );
+	return (
+		<Fragment>
+			<header className="App-header">
+        <div className="welcomeBox">
+        <h1>
+          Insta John Main Page
+        </h1>
+        </div>
+        <div className="discription">
+          This site is for people who know John
+          </div>
+       <div>   
+       <img src={personWithPhone} className='persononphone' alt='person with phone'/>
+       <img src={backGround2} className='Landingbackground' alt='landing background'/>
+       </div>
+      </header>
+		</Fragment>
+	);
 };
 
+
 export default Landing;
+
